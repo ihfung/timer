8288c47ed34ae99bdc4b8839bf9929df4424a802
ihleung <ihfungleung@gmail.com>
2024-05-07 15:55:19 -0400

    udpated

--- timer1.js
+++ timer1.js
@@ -1,7 +1,14 @@
+const input = process.argv.slice(2);
+
 const timer = function(input) {
-  setTimeout(() => {
-    console.log(`Timer ${input} is done!`);
-  }, input);
-}
+  for (let time of input) {
+    if (time >= 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, time * 1000);
+    }
+  }
+};
 
 
+timer(input);
