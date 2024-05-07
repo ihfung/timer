36c6b32b2c9b0bf7a0f9515fbce4759f35f1fe8c
ihleung <ihfungleung@gmail.com>
2024-05-07 16:03:12 -0400

    updated

--- timer1.js
+++ timer1.js
@@ -1,11 +1,11 @@
-const input = process.argv.slice(2);
+const input = process.argv.slice(2); // get the input from the command line
 
 const timer = function(input) {
-  for (let time of input) {
-    if (time >= 0) {
+  for (let time of input) { // loop through the input
+    if (time >= 0) { // check if the input is a positive number
       setTimeout(() => {
-        process.stdout.write('\x07');
-      }, time * 1000);
+        process.stdout.write('\x07'); // beep sound
+      }, time * 1000); //delay the beep sound by the input time
     }
   }
 };
